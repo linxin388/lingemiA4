f3034810f9b6c4c6da5b856362b0f7b128a19903
linxin388 <2323406443@qq.com>
2025-06-14 23:28:17 +0800

    # Read filenames from file_list and download each file

--- client.py
+++ client.py
@@ -4,6 +4,7 @@ import os
 import base64
 import time
 
+
 class UDPClient:
     def __init__(self, host, port, file_list):
         self.server_host = host
@@ -12,3 +13,11 @@ class UDPClient:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.settimeout(1)  # Initial timeout of 1 second
         self.max_retries = 5
+
+    def download_files(self):
+        try:
+            with open(self.file_list, 'r') as f:
+                files = [line.strip() for line in f.readlines() if line.strip()]
+
+            for filename in files:
+                self.download_file(filename)
